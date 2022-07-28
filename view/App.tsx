f767b5f1dcc7cd3c8c148975ad075751d568b109
草鞋没号 <308487730@qq.com>
2022-07-28 11:20:16 +0800

    chore: improve Table use-case

--- view/App.tsx
+++ view/App.tsx
@@ -1,30 +1,58 @@
 import { Button } from 'element-ui'
 import { Component } from 'vue'
-import { Table, TableProps } from '../components'
+import { Table, TableHandle, TableProps, TableQuery } from '../components'
 
 export interface RowRecord {
   name: string
   age: number
   gender: 0 | 1
+  date: number
 }
 
 const App: Component = {
   components: {
     [Table.name]: Table,
   },
+  created() {
+    this.tableHandle = {} as TableHandle
+
+    // 主动控制表格数据属性
+    // setInterval(() => this.data = this.data.map(d => Object.assign(d, { date: Date.now() })), 1000)
+  },
   data() {
     const data: RowRecord[] = [
-      { name: '张三', age: 23, gender: 1 },
-      { name: '阿宁', age: 24, gender: 0 },
+      { name: '张三', age: 23, gender: 1, date: Date.now() },
+      { name: '阿宁', age: 24, gender: 0, date: Date.now() },
     ]
+    const pagination = { currentPage: 1, pageSize: 10, total: 10 }
 
     return {
       data,
+      pagination,
     }
+  },
+  methods: {
+    query: async function query(args) {
+      console.log('-- 发起请求 --', JSON.parse(JSON.stringify(args)))
+
+      // 打断请求
+      // return
+
+      // 这里写请求
+      return {
+        data: this.data.map(d => Object.assign(d, { date: Date.now() })),
+        ...this.pagination,
+      }
+    } as TableQuery,
+    refresh() {
+      this.tableHandle.refresh()
+    },
   },
   render() {
     const tableProps: TableProps<RowRecord> = {
       data: this.data,
+      query: args => this.query(args),
+      handle: this.tableHandle,
       columns: [
         {
           label: '姓名',
@@ -44,11 +72,18 @@ const App: Component = {
           render: ({ row }) => row.gender === 1 ? '男' : '女',
         },
         {
+          label: '时间',
+          prop: 'date',
+          render: ({ row }) => new Date(row.date).toLocaleString(),
+        },
+        {
           label: '操作',
           prop: '操作',
-          render: () => <Button>查看</Button>,
+          render: () => <Button on-click={this.refresh}>刷新</Button>,
         },
       ],
+      // 关闭分页
+      // pagination: null,
     }
 
     return (
