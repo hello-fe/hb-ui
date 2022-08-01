f1bdc2808c753ea8b183380afbaea8150fa158c9
草鞋没号 <308487730@qq.com>
2022-08-01 10:47:42 +0800

    feat: layout

--- packages/element-ui/view/App.tsx
+++ packages/element-ui/view/App.tsx
@@ -1,11 +1,36 @@
 import { Component } from 'vue'
+import { routes } from './router'
 
 const App: Component = {
+  data() {
+    return {
+      openedMenus: [],
+    }
+  },
   render() {
     return (
-      <div class="app-vue">
-        <router-view />
-      </div>
+      <el-container class="app-vue">
+        <el-header>
+          Hello/hb-ui
+        </el-header>
+
+        <el-container>
+          <el-aside width="200px">
+            <el-menu default-active="1" class="el-menu-vertical-demo">
+              {routes.map((route, index) => (
+                <el-menu-item index={String(index + 1)}>
+                  <i class={route.meta?.icon || 'el-icon-menu'}></i>
+                  <router-link class="router-link" slot="title" to={route.path}>{route.name}</router-link>
+                </el-menu-item>
+              ))}
+            </el-menu>
+          </el-aside>
+
+          <el-main>
+            <router-view />
+          </el-main>
+        </el-container>
+      </el-container>
     )
   },
 }
