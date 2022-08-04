00bb2b18cb8657c036c8b62db44f95ee357e183e
younglei <youngleish@gmail.com>
2022-08-04 21:02:03 +0800

    antd:add components

---
+++ packages/antd/components/form/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FormAntd() {
+
+  return (
+    <div className="hb-ui-form">
+
+    </div>
+  )
+}
+
+export default FormAntd
---
+++ packages/antd/components/table/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TableAntd() {
+
+  return (
+    <div className="hb-ui-table">
+
+    </div>
+  )
+}
+
+export default TableAntd
