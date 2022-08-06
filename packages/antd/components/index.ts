9565112f70538b82b1431e8b0de2d507c4448bb1
younglei <youngleish@gmail.com>
2022-08-06 20:50:11 +0800

    chore(antd): rename

--- packages/antd/components/index.ts
+++ packages/antd/components/index.ts
@@ -2,4 +2,4 @@ export * from './form'
 export { default as Form } from './form'
 
 export * from './table'
-export { default as TableAntd } from './table'
+export { default as Table } from './table'
