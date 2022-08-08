84cfda467ce896102fa8df9ed7629fbfcedf12a8
younglei <youngleish@gmail.com>
2022-08-08 23:43:18 +0800

    chore(antd): use Form

--- packages/antd/view/form/index.tsx
+++ packages/antd/view/form/index.tsx
@@ -1,7 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+  Form,
+  FormProps,
+} from 'root/components'
 
 export default () => {
+
+  const onSubmit: FormProps['onSubmit'] = (values, form) => {
+    console.log(values, form)
+  }
+
+  const formProps: FormProps = {
+    onSubmit,
+    items: [
+      {
+        label: '姓名',
+        name: 'name',
+      },
+      {
+        label: '性别',
+        name: 'gender',
+        radioGroup: {
+          options: [
+            {
+              label: '男',
+              value: 1,
+            },
+            {
+              label: '女',
+              value: 0,
+            },
+          ],
+        },
+      },
+      {
+        label: '岗位',
+        name: 'department',
+        select: {
+          options: [
+            {
+              label: '前端',
+              value: 'front-end'
+            },
+            {
+              label: '产品',
+              value: 'product'
+            },
+          ],
+        },
+      },
+      {
+        label: '时间',
+        name: 'date',
+        rangePicker: {},
+      },
+      {
+        label: '状态',
+        name: 'status',
+        switch2: {
+          checkedChildren: '在职',
+          unCheckedChildren: '离职',
+          // Warning: [antd: Switch] `value` is not a valid prop, do you mean `checked`?
+          checked: undefined,
+        },
+      }
+    ],
+  }
+
   return (
-    <div>Form Component</div>
+    <div>
+      <Form {...formProps} />
+    </div>
   )
 }
