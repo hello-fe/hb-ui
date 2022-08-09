d980949c2052bd3f0fbd4a04b126bfb37240720b
草鞋没号 <308487730@qq.com>
2022-08-09 09:28:08 +0800

    feat(element-ui): router control

--- packages/element-ui/view/App.tsx
+++ packages/element-ui/view/App.tsx
@@ -7,20 +7,36 @@ const App: Component = {
       openedMenus: [],
     }
   },
+  methods: {
+    forward(path: string) {
+      if (this.$route.path === path) return
+      this.$router.push(path)
+    }
+  },
   render() {
+
     return (
       <el-container class="app-vue">
         <el-header>
-          Hello/hb-ui
+          <a
+            style={'cursor:pointer;' + (this.$route.path === '/' ? 'color:#409EFF;' : '')}
+            on-click={() => this.forward('/')}
+          >
+            Hello/hb-ui
+          </a>
         </el-header>
 
         <el-container>
           <el-aside width="200px">
             <el-menu default-active="1" class="el-menu-vertical-demo">
               {routes.map((route, index) => (
-                <el-menu-item index={String(index + 1)}>
+                <el-menu-item
+                  index={String(index + 1)}
+                  on-click={() => this.forward(route.path)}
+                >
                   <i class={route.meta?.icon || 'el-icon-menu'}></i>
-                  <router-link class="router-link" slot="title" to={route.path}>{route.name}</router-link>
+                  {/* <router-link class="router-link" slot="title" to={route.path}>{route.name}</router-link> */}
+                  <a>{route.name}</a>
                 </el-menu-item>
               ))}
             </el-menu>
